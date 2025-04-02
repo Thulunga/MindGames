79d04a22408520cd762d17bf2f994f20f567145b
Thulunga Basumatary <thulunga.basumatary@soti.net>
2025-04-02 13:21:00 +0530

    initial game setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WordleComponent } from './wordle/wordle.component';
+import { SudokuComponent } from './sudoku/sudoku.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'wordle', component: WordleComponent },
+  { path: 'sudoku', component: SudokuComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WordleComponent } from './wordle/wordle.component';
+import { SudokuComponent } from './sudoku/sudoku.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'wordle', component: WordleComponent },
+  { path: 'sudoku', component: SudokuComponent }
+];
